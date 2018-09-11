f716d643da84bcd9ab6a6e1bf892f835da6d262f
41389363@qq.com <41389363@qq.com>
2018-09-11 11:33:56 +0800

    提交jcenter配置

---
+++ app/src/main/java/com/smartcity/commonbase/widget/pagestatus/test/MainActivity.java
@@ -0,0 +1,28 @@
+package com.smartcity.commonbase.widget.pagestatus.test;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.smartcity.commonbase.widget.pagestatus.Builder;
+import com.smartcity.commonbase.widget.pagestatus.OnErrorClickListener;
+import com.smartcity.commonbase.widget.pagestatus.PageStatusHelper;
+import com.smartcity.commonbase.widget.pagestatus.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PageStatusHelper helper;
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+}
